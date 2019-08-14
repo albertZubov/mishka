5f0cac9ea75c4557b6fd3e7a99bc52768af39bd7
Albert <42963174+albertZubov@users.noreply.github.com>
2019-08-14 10:20:35 +0300

    Финальные исправления, правки по все проекту, без РР

--- gulpfile.js
+++ gulpfile.js
@@ -86,17 +86,8 @@ gulp.task("html", function() {
 gulp.task("copy", function() {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
-    "source/img/**.{png,jpg,webp}",
-    "source/img/logo-mobile.svg",
-    "source/img/logo-tablet.svg",
-    "source/img/logo-desktop.svg",
-    "source/img/bg-zigzag-fill.svg",
-    "source/img/bg-zigzag-line.svg",
-    "source/img/icon-right-arrow.svg",
-    "source/img/icon-left-arrow.svg",
-    "source/img/icon-menu-open.svg",
-    "source/img/icon-menu-close.svg",
-    "source/img/icon-tick.svg"
+    "source/img/imgInline/*",
+    "source/img/**.{png,jpg,webp}"
     ], {
       base: "source"
     })
--- source/js/navigation.window.js
+++ source/js/navigation.window.js
@@ -1,14 +1,10 @@
 var navMain = document.querySelector('.main-nav');
-        var navToggle = document.querySelector('.main-nav__toggle');
+var navToggle = document.querySelector('.main-nav__toggle');
+var notJs = document.querySelector('.page');
 
-        navMain.classList.remove('main-nav--nojs');
+notJs.classList.remove('page--nojs');
 
-        navToggle.addEventListener('click', function() {
-          if (navMain.classList.contains('main-nav--closed')) {
-            navMain.classList.remove('main-nav--closed');
-            navMain.classList.add('main-nav--opened');
-          } else {
-            navMain.classList.add('main-nav--closed');
-            navMain.classList.remove('main-nav--opened');
-          }
-        });
+navToggle.addEventListener('click', function() {
+  navMain.classList.toggle('main-nav--closed');
+});
+
